0e03b7c4ae5a5e748693d3b9642435c5d81e1fcc
Austin Garrard <austin.w.garrard@gmail.com>
2022-12-25 11:52:55 -0600

    pin provider versions

---
+++ tf/gcp/service_identity/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">=1.3"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.44.1"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.16.0"
+    }
+  }
+}
