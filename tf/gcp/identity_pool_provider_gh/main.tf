b7b6907e00a2be78e9748a779ffa9194b5b5ebe2
Austin Garrard <austin.w.garrard@gmail.com>
2022-12-25 13:37:10 -0600

    update pool provider params

--- tf/gcp/identity_pool_provider_gh/main.tf
+++ tf/gcp/identity_pool_provider_gh/main.tf
@@ -1,23 +1,27 @@
-variable "workload_identity_pool_id" {
+variable "id" {
   type = string
 }
 
-variable "name" {
+variable "pool_workload_identity_pool_id" {
+  type = string
+}
+
+variable "pool_name" {
   type = string
 }
 
 variable "repos" {
-  type = list(string)
+  type    = list(string)
   default = []
 }
 
 
 resource "google_iam_workload_identity_pool_provider" "github" {
-  workload_identity_pool_provider_id = "github"
-  workload_identity_pool_id          = var.workload_identity_pool_id
-  attribute_mapping = {
-    "google.subject" = "assertion.sub"
-    "attribute.actor" = "assertion.actor"
+  workload_identity_pool_provider_id = var.id
+  workload_identity_pool_id          = var.pool_workload_identity_pool_id
+  attribute_mapping                  = {
+    "google.subject"       = "assertion.sub"
+    "attribute.actor"      = "assertion.actor"
     "attribute.repository" = "assertion.repository"
   }
   oidc {
@@ -30,5 +34,8 @@ output "name" {
 }
 
 output "principals" {
-  value = {for repo in var.repos: repo => "principalSet://iam.googleapis.com/${var.name}/attribute.repository/${repo}"}
+  value = {
+    for repo in var.repos :
+    repo => "principalSet://iam.googleapis.com/${var.pool_name}/attribute.repository/${repo}"
+  }
 }
